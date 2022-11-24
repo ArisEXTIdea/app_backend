60f6ce95aeb381d138a8b1effc00de4a3b1bca82
Muhammad Aris Widaryani <arisk697@gmail.com>
2022-11-24 07:50:26 +0700

    Update Home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,15 +2,10 @@
 
 namespace App\Controllers;
 
-use APP\Models\FimonM;
 
 class Home extends BaseController
 {
 
-    public function __construct()
-    {
-        $this->FimonM = new FimonM;
-    }
 
     public function index()
     {
